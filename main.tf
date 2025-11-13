d7cfe2f7e4c9200a6fad6d805b74e86a30c36c2a
Stephen Revel <Stephen.Revel@outlook.com>
2025-11-13 08:53:36 -0700

    commit

--- main.tf
+++ main.tf
@@ -8,3 +8,22 @@ resource "azurerm_resource_group" "nnl_uswest3" {
     project     = "NNL"
   }
 }
+
+resource "azurerm_virtual_network" "nnl_vnet" {
+  name                = "NNLVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.nnl_uswest3.location
+  resource_group_name = azurerm_resource_group.nnl_uswest3.name
+
+  tags = {
+    environment = "dev"
+    project     = "NNL"
+  }
+}
+
+resource "azurerm_subnet" "nnl_subnet" {
+  name                 = "NNLSubnet"
+  resource_group_name  = azurerm_resource_group.nnl_uswest3.name
+  virtual_network_name = azurerm_virtual_network.nnl_vnet.name
+  address_prefixes     = [cidrsubnet(azurerm_virtual_network.nnl_vnet.address_space[0], 8, 1)]
+}
